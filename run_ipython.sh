6fb16dea7da4344d6c8d35e9f1c837fea177b2a4
mattasmith <matt.a.smith@gmail.com>
2016-01-03 19:07:18 -0800

    Updated directory links so repo can run from any directory
    Added extensions to server_ext_full (could not find extension otherwise)
    Changed the notebook directory to the notebooks directory in the repo
    Changed the log directory to the logs directory in the repo

--- run_ipython.sh
+++ run_ipython.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python -m IPython notebook --notebook-dir=~/insight --NotebookApp.server_extensions="['server_ext_full']" &> log_$(date +%s).txt
+python -m IPython notebook --notebook-dir=./notebooks --NotebookApp.server_extensions="['extensions.server_ext_full']" &> logs/log_$(date +%s).txt
